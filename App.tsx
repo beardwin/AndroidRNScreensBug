660e13a90fe85d88da9f6ec19e1bb00cfbe281ae
Ryan Baldwin <ryan.baldwin@shopify.com>
2020-09-12 13:44:15 -0400

    example app for title bug

--- App.tsx
+++ App.tsx
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StackScreenProps } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "react-native-screens/native-stack";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { enableScreens } from "react-native-screens";
 
+enableScreens();
+
+type NavigatorParams = {
+  Home: undefined;
+  PooFarts: undefined;
+};
+
+const HomeScreen = ({
+  navigation,
+}: StackScreenProps<NavigatorParams, "Home">) => (
+  <View style={styles.screen}>
+    <Text>Home Screen</Text>
+    <Button
+      onPress={() => {
+        navigation.navigate("PooFarts");
+      }}
+      title="Pop a poo fart"
+    />
+  </View>
+);
+
+const PooFartsScreen = ({
+  navigation,
+}: StackScreenProps<NavigatorParams, "PooFarts">) => {
+  useEffect(() => {
+    navigation.setOptions({
+      title: "Fiddlesticks",
+      // uncomment below, and the title will no longer appear
+      // headerLeft: () => <Button title="Adios!" onPress={navigation.goBack} />,
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.screen}>
+      <Text>Poo Farts</Text>
+    </View>
+  );
+};
+
+const Stack = createNativeStackNavigator<NavigatorParams>();
+
+const Navigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen
+      name="PooFarts"
+      component={PooFartsScreen}
+      options={{ stackPresentation: "modal" }}
+    />
+  </Stack.Navigator>
+);
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
